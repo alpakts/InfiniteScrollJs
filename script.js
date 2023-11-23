d5988809456e72869fdf55e4415b920f9e818391
alpakts <ultrafakto@gmail.com>
2023-11-23 13:25:32 +0300

    bug fix

--- script.js
+++ script.js
@@ -1,54 +1,78 @@
-const imageContainer = document.querySelector("#image-container");
-const count = 30;
-const apiKey = "PeC8uf2IiHe_ph90j8hwNXKiPGZfiQxvrTKfgb5Hx-s";
-const loader=document.querySelector("#loader")
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
-var photosArray = [];
-const ApiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
-const getPhotos = async () => {
-  try {
-    const response = await fetch(ApiUrl);
-    
-    if (response.status=200) {
-        photosArray = await response.json();
-    }
-    displayPhotos();
-  } catch (error) {
-    alert(error.message)
+let photosArray = [];
+
+// Unsplash API
+const count = 30;
+// Normally, don't store API Keys like this, but an exception made here because it is free, and the data is publicly available!
+const apiKey = "PeC8uf2IiHe_ph90j8hwNXKiPGZfiQxvrTKfgb5Hx-s";
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
+
+// Check if all images were loaded
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
   }
-};
-const displayPhotos = () => {
-    imagesLoaded=0;
-    totalImages =photosArray.length;
-    
-  photosArray.forEach((element) => {
-    const item = document.createElement("a");
-    item.setAttribute("href", element.links.html);
-    item.setAttribute("target", "_blank");
-    const img = document.createElement("img");
-    img.setAttribute("src", element.urls.regular);
-    img.setAttribute("alt", element.alt_description);
-    img.setAttribute("title", element.alt_description);
+}
+
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos() {
+  imagesLoaded = 0;
+  totalImages = photosArray.length;
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to full photo
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    // Create <img> for photo
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    // Event Listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded);
+    // Put <img> inside <a>, then put both inside imageContainer Element
     item.appendChild(img);
     imageContainer.appendChild(item);
   });
-};
-window.addEventListener("scroll", () => {
-  if (
-    window.innerHeight + window.scrollY >=
-    document.body.offsetHeight - 1000 
-  ) {
-    ready=false;
+}
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
+
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
     getPhotos();
   }
 });
-const imageLoaded=()=>{
-    imagesLoaded++;
-    if (imagesLoaded===totalImages) {
-        ready=true
-        loader.hidden=true
-    }
-}
+
+// On Load
 getPhotos();
